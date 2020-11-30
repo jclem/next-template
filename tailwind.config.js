bb6e0450e395c78396ce2fe2b7073ca42157bd4d
Jonathan Clem <jonathan@jclem.net>
2020-11-30 10:22:16 -0500

    Update dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,7 @@
-const openColor = require('open-color')
-
 module.exports = {
   purge: ['./styles/**/*', './components/**/*', './pages/**/*'],
   theme: {
     extend: {
-      colors: () => {
-        const shadeIndices = [
-          '50',
-          '100',
-          '200',
-          '300',
-          '400',
-          '500',
-          '600',
-          '700',
-          '800',
-          '900'
-        ]
-
-        return Object.entries(openColor).reduce((colors, [name, shades]) => {
-          if (['white', 'black'].includes(name)) {
-            colors[name] = {default: shades}
-          } else {
-            colors[name] = Object.values(shades).reduce((shades, shade, i) => {
-              shades[shadeIndices[i]] = shade
-              return shades
-            }, {})
-          }
-
-          return colors
-        }, {})
-      },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
         mono: ['Fira Code', 'monospace']
@@ -38,9 +9,5 @@ module.exports = {
     }
   },
   variants: {},
-  plugins: [],
-  future: {
-    purgeLayersByDefault: true,
-    removeDeprecatedGapUtilities: true
-  }
+  plugins: []
 }
